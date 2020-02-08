0fdb41e0f5d15044c62a74474a73c8a54e7c274b
Leshey <leshey95@gmail.com>
2020-02-08 17:34:10 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace OddEvenNumbersApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число:");
+            int a = Console.Read();
+            int b = a % 2;
+            if (b == 0)
+            {
+                Console.WriteLine("Число четное!");
+            }
+            else
+            {
+                Console.WriteLine("Число нечетное!");
+            }
+            
+
+        }
+    }
+}
